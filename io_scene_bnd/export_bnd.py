2fab4954610116e0e34aedfee70b57d1b3093100
aaro4130 <aaro4130@gmail.com>
2016-08-08 15:42:14 -0400

    Fix for 'context is incorrect' on export

--- io_scene_bnd/export_bnd.py
+++ io_scene_bnd/export_bnd.py
@@ -419,8 +419,9 @@ def save(operator,
     
     # set object modes
     for ob in context.scene.objects:
-      context.scene.objects.active = ob
-      bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+      if ob.type == 'MESH':
+        context.scene.objects.active = ob
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
     
     # set globals
     global apply_modifiers_G
